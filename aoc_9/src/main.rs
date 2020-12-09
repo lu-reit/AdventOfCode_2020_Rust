990f538fe1f7ad75b364a639d40371be10aa89d6
Lukas Reitemeyer <lukas@skoni.de>
2020-12-09 13:27:28 +0100

    Day 9: All Done

--- aoc_9/src/main.rs
+++ aoc_9/src/main.rs
@@ -1,9 +1,9 @@
 use std::fs;
 use std::time::Instant;
 
+// Use an optimized HashSet
 extern crate fxhash;
 use fxhash::FxHashSet;
-
 
 fn main() {
     let (file, pre_size) = ("input", 25);
@@ -15,11 +15,18 @@ fn main() {
     let p1_result = p1_result_opt.expect("Part1 failed. No point to continue");
     println!("Result part1: {:?}", p1_result);
     println!("Time part1: {:?}", p1_time);
-    
+
+    let p1_alt_timer = Instant::now();
+    let p1_alt_result_opt = part1_alt(pre_size, &numbers);
+    let p1_alt_time = p1_alt_timer.elapsed();
+    let p1_alt_result = p1_alt_result_opt.expect("Part1 failed. No point to continue");
+    println!("Result part1_alt: {:?}", p1_alt_result);
+    println!("Time part1_alt: {:?}", p1_alt_time);
+
     let p2_timer = Instant::now();
-    let p2_result_opt = part1_alt(pre_size, &numbers);
+    let p2_result_opt = part2(p1_result, &numbers);
     let p2_time = p2_timer.elapsed();
-    let p2_result = p2_result_opt.expect("Part2 failed. No point to continue");
+    let p2_result = p2_result_opt.expect("Part1 failed. No point to continue");
     println!("Result part2: {:?}", p2_result);
     println!("Time part2: {:?}", p2_time);
 }
@@ -39,8 +46,8 @@ fn part1(pre_size: usize, numbers: &[u64]) -> Option<u64> {
 }
 
 // Alternative Solution, using a HashSet, with a complexity of O(n).
-// However, for the given input this ends being slower than O(n^2) solution 
-// above (Much slower if you use the HashSet from std-library)
+// However, for the given input this ends being slower than the O(n^2) solution 
+// above (Much slower if you use the HashSet from the standart library)
 fn part1_alt(pre_size: usize, numbers: &[u64]) -> Option<u64> {
     let mut batch_set: FxHashSet<u64> = FxHashSet::default();
 
@@ -54,12 +61,26 @@ fn part1_alt(pre_size: usize, numbers: &[u64]) -> Option<u64> {
                 batch_set.clear();
                 continue 'b_loop }
         }
-
         return Some(batch[pre_size]);
     }
     None        
 }
+ 
+fn part2(target: u64, numbers: &[u64]) -> Option<u64> {
+    // Do not need to consider any values which have indices greater than target
+    let target_ind: usize = numbers.iter().position(|&x| x == target).unwrap();
+    let to_consider = &numbers[..target_ind];
 
+    for window_size in 2..to_consider.len() {
+        for batch in to_consider.windows(window_size) {
+            if target == batch.iter().sum::<u64>() {
+                return Some(batch.iter().max().unwrap() 
+                    + batch.iter().min().unwrap());
+            }
+        }
+    }
+    None
+}
 
 fn read_numbers(filename: &str) -> Vec<u64> {
     fs::read_to_string(filename).unwrap()
