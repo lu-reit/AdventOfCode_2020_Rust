66b7c96032cbbc74657219b71437d461150bb418
Lukas Reitemeyer <lukas@skoni.de>
2020-12-10 12:41:23 +0100

    Day 10: Part 2 done

--- aoc_10/src/main.rs
+++ aoc_10/src/main.rs
@@ -1,16 +1,23 @@
 use std::fs;
+use std::time::Instant;
 
 fn main() {
     let mut numbers = read_numbers("input");
-    let p1_result = part1(&mut numbers);
-    println!("Part1 result: {}", p1_result);
-}
 
-fn part1(numbers: &mut Vec<u32>) -> u32 {
+    // Sort the Vec and add the input/output joltages.
+    // This makes both parts much easier to solve.
     numbers.push(0);
     numbers.sort();
-    println!("{:?}", numbers);
-    let (mut ones, mut threes) = (0, 1);
+    numbers.push(numbers.iter().last().unwrap() + 3);
+
+    run_and_time(1, &numbers, &part1);
+    run_and_time(2, &numbers, &part2);
+}
+
+// Just count the differences in the sorted list
+fn part1(numbers: &[u32]) -> u64 {
+    let (mut ones, mut threes) = (0, 0);
+
     for window in numbers.windows(2) {
         match window[1] - window[0] {
             3 => { threes += 1; }
@@ -18,10 +25,39 @@ fn part1(numbers: &mut Vec<u32>) -> u32 {
             _ => { panic!("Part1: Difference between adapters greater than 3"); }
         }
     }
-    println!("ones: {}, threes: {}", ones, threes);
     ones * threes
 }
-    
+
+fn part2(numbers: &[u32]) -> u64 {
+    let len = numbers.len();
+
+    // Holds the number of possible paths that still can be taken
+    let mut paths: Vec<u64> = vec![1; len];
+    // Deal with the edgecase number.len() - 3 
+    if numbers[len - 1] - numbers[len - 3] <= 3 { paths[len - 3] = 2; }
+
+    // Iterate through numbers/paths in reverse and sum over the paths
+    // for the previous adapters which are in range
+    for i in (0..len - 3).rev() {
+        let mut path_sum = paths[i + 1];
+        if numbers[i + 2] - numbers[i] <= 3 { path_sum += paths[i + 2] }
+        if numbers[i + 3] - numbers[i] <= 3 { path_sum += paths[i + 3] }
+        paths[i] = path_sum;
+    } 
+    paths[0]
+} 
+
+// Playing around with functions as arguments
+fn run_and_time(part: u8, numbers: &[u32], f: &dyn Fn(&[u32]) -> u64) {
+    let mut timer = Instant::now();
+    let result = f(numbers);
+    let time = timer.elapsed();
+    println!("Result part{}: {}", part, result);
+    // Printing time as debug automatically converts time into 
+    // the appropiate unit
+    println!("Time part{}: {:?}", part, time);
+}
+
 
 fn read_numbers(filename: &str) -> Vec<u32> {
     fs::read_to_string(filename).unwrap()
