2b788219fb33d808aeace270ca7481c5f9b5b48e
Lukas Reitemeyer <lukas@skoni.de>
2020-12-13 16:30:59 +0100

    Day 13 Part2: added sorting

--- aoc_13/src/main.rs
+++ aoc_13/src/main.rs
@@ -1,4 +1,5 @@
 use std::fs;
+use std::time::Instant;
 
 type ID = u64;
 type Delta = u64;
@@ -6,12 +7,17 @@ type Delta = u64;
 fn main() {
     let (target, mut ids) = read_file("input");
 
+    let p1_timer = Instant::now();
     let p1_result = part1(target, &ids);
+    let p1_time = p1_timer.elapsed();
     println!("Part1 result: {}", p1_result);
-    println!("{:?}", ids);
+    println!("Part1 time: {:?}", p1_time);
 
+    let p2_timer = Instant::now();
     let p2_result = part2(&mut ids);
+    let p2_time = p2_timer.elapsed();
     println!("Part2 result: {}", p2_result);
+    println!("Part2 time: {:?}", p2_time);
 }
 
 fn part1(target: u64, ids: &[(Delta, ID)]) -> u64 {
@@ -24,15 +30,18 @@ fn part1(target: u64, ids: &[(Delta, ID)]) -> u64 {
 }
  
 fn part2(ids: &mut [(Delta, ID)]) -> u64 {
+    ids.sort_by(|(_, x), (_, y)| y.cmp(x));
+    let offset = ids[0].0;
+
     let mut base = ids[0].1;
     let mut time = 0;
     for (d, id) in ids.iter().skip(1) {
-        while (time + d) % id != 0 {
+        while (time + d - offset) % id != 0 {
             time += base;
         }
         base *= id;
     }
-    return time;
+    return time - offset;
 }
 
 fn read_file(filename: &str) -> (u64, Vec<(Delta, ID)>) {
