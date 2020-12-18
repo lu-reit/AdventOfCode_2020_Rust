9aff90c812d433bb46ccbd7f994563cd10f761ab
Lukas Reitemeyer <lukas@skoni.de>
2020-12-18 22:53:41 +0100

    Day 18, part2 done

--- aoc_18/src/main.rs
+++ aoc_18/src/main.rs
@@ -1,25 +1,26 @@
 use std::fs::File;
 use linereader::LineReader;
 use std::time::Instant;
-
  
 type Num = u64; 
  
-enum Op {
-    Scalar(Num),
-    Add(Box<Op>, Box<Op>),
-    Mul(Box<Op>, Box<Op>)
-}
-
-
 fn main() {
+    let parse_timer = Instant::now();
     let lines = read_file("input");
+    let parse_time = parse_timer.elapsed();
+    println!("Parse time: {:?}", parse_time);
 
     let p1_timer = Instant::now();
     let p1_result = part1(&lines);
     let p1_time = p1_timer.elapsed();
     println!("Part1 result: {}", p1_result);
     println!("Part1 time: {:?}", p1_time);
+
+    let p2_timer = Instant::now();
+    let p2_result = part2(&lines);
+    let p2_time = p2_timer.elapsed();
+    println!("Part2 result: {}", p2_result);
+    println!("Part2 time: {:?}", p2_time);
 }
 
 
@@ -54,8 +55,46 @@ fn eval1(line: &[u8]) -> (Num, usize) {
     }
 }
 
-fn eval2(line: &[u8]) -> (Num, usize) {
+fn part2(lines: &[Vec<u8>]) -> Num {
+    let mut sum = 0;
+    for line in lines {
+        let (val, _ ) = eval2(0, &line);
+        sum += val;
+    }
+    sum
+}
 
+fn eval2(pos: usize, line: &[u8]) -> (Num, usize) {
+    let (mut lval, mut op_pos) = if line[pos] == b'(' {
+        eval2(pos + 1, &line)
+    } else {
+        (line[pos] as Num, pos + 1)
+    };
+
+    let mut to_mul: [Num; 6] = [1; 6];
+    to_mul[0] = lval;
+    let mut to_mul_i = 0;
+
+    loop {
+        // Get the value for the right-hand side and the position 
+        // of the next left-hand side
+        let rpos = op_pos + 1;
+        let (rval, new_pos) = if line[rpos] == b'(' {
+            eval2(rpos + 1, &line)
+        } else {
+            (line[rpos] as Num, rpos + 1)
+        };
+        if line[op_pos] == b'+' {
+            to_mul[to_mul_i] = to_mul[to_mul_i] + rval;
+        } else { 
+            to_mul_i += 1;
+            to_mul[to_mul_i] = rval;
+        }
+        lval = rval;
+        op_pos = new_pos;
+        if line[op_pos] == b')' { break }
+    }
+    (to_mul.iter().product(), op_pos + 1)
 }
 
 
@@ -64,13 +103,15 @@ fn read_file(filename: &str) -> Vec<Vec<u8>>  {
     let mut lines: Vec<Vec<u8>> = Vec::new();
 
     lreader.for_each(|line| {
+        // Read the line as u8, remove white-space and map ascii-digits
+        // to int-values
         let mut tokens: Vec<u8> = line.into_iter()
             .filter(|chr| !chr.is_ascii_whitespace())
             .map(|chr| if chr.is_ascii_digit() { *chr - 48 } else { *chr })
             .collect();
-        tokens.push(b')'); // Cough Cough :D
+        tokens.push(b')'); // Add a delimiter :D
         lines.push(tokens);
-        Ok(true)
+        Ok(true) // No clue, but linereader requires this
     });
     lines
 }
