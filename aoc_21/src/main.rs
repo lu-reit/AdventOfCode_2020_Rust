be517a698becebaae44445dac8a165f14bf8d2f5
Lukas Reitemeyer <lukas@skoni.de>
2021-01-08 17:59:30 +0100

    Day 21: Finished parsing

--- aoc_21/src/main.rs
+++ aoc_21/src/main.rs
@@ -1,4 +1,6 @@
 use std::fs;
+use std::fs::File;
+use std::io::Read;
 use std::collections::HashMap;
 use std::collections::HashSet;
 use regex::Regex;
@@ -8,31 +10,64 @@ fn main() {
     read_allergens("test");
 }
  
-struct Foods<'a> {
-    ingreds: Vec<String>,
-    allergens: Vec<String>,
-    fmap: HashMap<&'a str, Vec<HashSet<&'a str>>>,
-    unknowns: HashSet<&'a str>
+type BString = Vec<u8>;
+type FoodMap = HashMap<BString, Vec<HashSet<BString>>>;
+
+fn b_to_s(bstring: &[u8]) -> String {
+    String::from_utf8_lossy(bstring).to_string()
 }
 
-impl<'a> Foods<'a> {
-    fn new() -> Foods<'a> {
+struct Foods {
+    ingreds: HashMap<BString, usize>,
+    fmap: FoodMap,
+}
+
+impl Foods {
+    fn new() -> Foods {
         Foods { 
-            ingreds: Vec::new(),
-            allergens: Vec::new(),
+            ingreds: HashMap::new(),
             fmap: HashMap::new(),
-            unknowns: HashSet::new(),
         }
     }
 }
 
 fn read_allergens(filename: &str) {
-    let mut buffer = fs::read_to_string(filename).unwrap();
-    //let mut foodmap: FoodMap = HashMap::new();
-    let mut ingreds: HashSet<String> = HashSet::new();
-    for line in buffer.trim().split('\n') {
-        
+    let mut buffer = read_ascii_trim(filename);
+    println!("{}", b_to_s(&buffer));
+    println!("{}", buffer.len());
+    let mut foods = Foods::new();
+
+    for line in buffer.split(|&c| c == b'\n') {
+        let mut parts = line.split(|&c| c == b'(');
+        let mut ingred_txt = parts.next().unwrap();
+        ingred_txt = &ingred_txt[..(ingred_txt.len() - 1)];
+
+        let mut allergen_txt = parts.next().unwrap();
+        allergen_txt = &allergen_txt[9..(allergen_txt.len() - 1)];
+
+        let mut cur_ingreds: HashSet<BString> = HashSet::new();
+        for ingred in ingred_txt.split(|&c| c.is_ascii_whitespace()) {
+            let i_vec = ingred.to_vec();
+            let mut entry = foods.ingreds.entry(i_vec).or_insert(0);
+            *entry += 1;
+            cur_ingreds.insert(ingred.to_vec());
+        }
+
+        for allergen in allergen_txt.split(|&c| c.is_ascii_whitespace()) {
+            let a_vec = allergen.to_vec();
+            let mut entry = foods.fmap.entry(a_vec).or_insert(Vec::new());
+            entry.push(cur_ingreds.clone());
+        }
     }
-    println!("{}", buffer);
+    for (ingred, count) in foods.ingreds.iter() {
+        println!("Ingredient: {};\t count: {}", b_to_s(&ingred), count);
+    }
 }
 
+fn read_ascii_trim(filename: &str) -> BString {
+    let mut file = File::open(filename).unwrap();
+    let mut buffer: Vec<u8> = Vec::new();
+    file.read_to_end(&mut buffer).unwrap();
+    if buffer[buffer.len() - 1] == b'\n' { buffer.pop(); }
+    buffer
+}
